6cf0e5104be3d04fbac99dce2ffc77d7e0025d04
Vittal <vittal.krishnasamy@gmail.com>
2014-04-30 16:03:15 +0530

    changed the install scirpt

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 #------------------------------------------------------------------------------
 #          FILE:  install.sh
 #   DESCRIPTION:  Bootstraps the installation of boxen and links dotfiles
-#        AUTHOR:  Adam Walz <adam@adamwalz.net>
+#        AUTHOR:  Adam Walz <adam@adamwalz.net> adapted to vittal's requirement
 #       VERSION:  1.0.0
 #------------------------------------------------------------------------------
 
 sudo mkdir -p /opt/boxen
 sudo chown ${USER}:staff /opt/boxen
-git clone https://github.com/adamwalz/my-boxen /opt/boxen/repo
+git clone https://github.com/vkrishnasamy/vkboxen /opt/boxen/repo
 /opt/boxen/repo/script/boxen --srcdir ${HOME}/Developer
